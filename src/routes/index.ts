493426f6460d6d5e2e13f5f99554436642ddf42a
irinazelenko <irina.z@lomray.com>
2025-11-08 23:24:30 +0400

    chore: update comments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -16,32 +16,33 @@ import {
 
 const router = Router();
 
-// METRICS ROUTES, Query params: from, to
 /**
+ * METRICS ROUTES, Query params: from, to
  * GET /api/metrics/${periodType}
  */
 router.get("/metrics/daily", getMetricsDaily);
 router.get("/metrics/weekly", getMetricsWeekly);
 router.get("/metrics/monthly", getMetricsMonthly);
 
-// WEIGHT ZONE ROUTES, Query params: from, to
 /**
+ * WEIGHT ZONE ROUTES, Query params: from, to
  * GET /api/weight-zone/${periodType}
  */
 router.get("/weight-zone/daily", getWeightZoneDaily);
 router.get("/weight-zone/weekly", getWeightZoneWeekly);
 router.get("/weight-zone/monthly", getWeightZoneMonthly);
 
-// GEO STATE ROUTES, Query params: from, to, state (optional)
+
 /**
+ * GEO STATE ROUTES, Query params: from, to, state (optional)
  * GET /api/geo/state/${periodType}
  */
 router.get("/geo/state/daily", getGeoStateDaily);
 router.get("/geo/state/weekly", getGeoStateWeekly);
 router.get("/geo/state/monthly", getGeoStateMonthly);
 
-// GEO COUNTY ROUTES, Query params: from, to, state (optional - REQUIRED for drilldown)
 /**
+ * GEO COUNTY ROUTES, Query params: from, to, state (optional - REQUIRED for drilldown)
  * GET /api/geo/county/${periodType}
  */
 router.get("/geo/county/daily", getGeoCountyDaily);
